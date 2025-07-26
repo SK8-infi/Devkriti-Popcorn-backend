37c4b9eac01244738a2c9a5ce0c78e6af8c15335
SK8-infi <shivansh.katiyar1712@gmail.com>
2025-07-27 03:38:35 +0530

    multi

--- models/Theatre.js
+++ models/Theatre.js
@@ -4,7 +4,14 @@ const theatreSchema = new mongoose.Schema({
   name: { type: String, required: true },
   city: { type: String, required: true },
   admin: { type: String, ref: "User", required: true }, // Clerk/Mongo user id
-  layout: { type: [[Number]], default: Array(8).fill().map(() => Array(10).fill(1)) }
+  layout: { type: [[Number]], default: Array(8).fill().map(() => Array(10).fill(1)) },
+  rooms: [
+    {
+      name: { type: String, required: true },
+      type: { type: String, required: true },
+      layout: { type: [[Number]], required: true }
+    }
+  ]
 });
 
 const Theatre = mongoose.model("Theatre", theatreSchema);
